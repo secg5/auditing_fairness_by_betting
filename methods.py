4c4ae7a1960b11067ce29eb23f290c3f6cc4abbf
Ben Chugg <ben.ih.chugg@gmail.com>
2023-05-16 12:07:43 -0400

    more exps

--- methods.py
+++ methods.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import os 
 import pickle
+from glob import glob
 from scipy.stats import permutation_test
 
 def test_by_betting(seq1, seq2, alpha=0.05): 
@@ -32,19 +33,41 @@ def test_by_betting(seq1, seq2, alpha=0.05):
     
     return wealth_hist, 'sustain'
 
-def betting_experiment(seq1, seq2, alphas, iters=10): 
-
+def betting_experiment(seq1, seq2, alphas, iters=10, shift_time=None): 
+    
     results = []
+    rejections = []
+    s1, s2 = seq1, seq2
     for _ in range(iters): 
-        np.random.shuffle(seq1)
-        np.random.shuffle(seq2)
-        taus = []
-        for alpha in alphas: 
-            wealth, _ = test_by_betting(seq1, seq2, alpha=alpha)
-            taus.append(len(wealth))
-        results.append(taus)
+        if shift_time != None: 
+            s1, s2 = shuffle_with_shift(seq1, seq2, shift_time)
+        else: 
+            np.random.shuffle(s1)
+            np.random.shuffle(s2)
         
-    return results
+        taus = []
+        rejects = []
+        for alpha in alphas: 
+            wealth, reject = test_by_betting(s1, s2, alpha=alpha)
+            taus.append(len(wealth))
+            rejects.append(True if reject == 'reject' else False)
+        results.append(taus)
+        rejections.append(rejects)
+        
+    return results, rejections
+
+def shuffle_with_shift(seq1, seq2, shift_time): 
+
+    s1_pre, s1_post = seq1[:shift_time], seq1[shift_time:]
+    s2_pre, s2_post = seq2[:shift_time], seq2[shift_time:]
+    np.random.shuffle(s1_pre), np.random.shuffle(s1_post)
+    np.random.shuffle(s2_pre), np.random.shuffle(s2_post)
+    s1 = np.concatenate((s1_pre, s1_post))
+    s2 = np.concatenate((s2_pre, s2_post))
+    
+    return s1, s2
+        
+
             
 def test_stat(x, y, axis):
     return np.mean(x, axis=axis) - np.mean(y, axis=axis)
@@ -73,16 +96,19 @@ def seq_perm_test(seq1, seq2, p=0.05, k=100, bonferroni=False):
 def seq_perm_test_experiment(seq1, seq2, alphas, iters=10, k=100, bonferroni=False): 
     
     results = []
+    rejections = []
     for _ in range(iters): 
-        taus = []
+        taus, rejects = [], []
         np.random.shuffle(seq1)
         np.random.shuffle(seq2)
         for alpha in alphas: 
-            steps, _ = seq_perm_test(seq1, seq2, p=alpha, k=k, bonferroni=bonferroni)
+            steps, reject = seq_perm_test(seq1, seq2, p=alpha, k=k, bonferroni=bonferroni)
             taus.append(steps)
+            rejects.append(True if reject == 'reject' else False)
         results.append(taus)
+        rejections.append(rejects)
         
-    return results
+    return results, rejections
     
 
 def get_mean_std(arr): 
@@ -90,13 +116,27 @@ def get_mean_std(arr):
 
 def plt_mean_std(ax, arr, alphas, label, color='navy', plot_std=True, **kwargs): 
     mean, std = get_mean_std(arr)
-    ax.plot(alphas, mean, lw=2, label=label, c=color, **kwargs)
+    ax.plot(alphas, mean, label=label, c=color, **kwargs)
     if plot_std: 
-        ax.fill_between(alphas, mean-std, mean+std, alpha=0.05, color=color)
+        ax.fill_between(alphas, mean-std/2, mean+std/2, alpha=0.05, color=color)
 
 def save_results(name, data): 
     
+    if not os.path.exists('data'): 
+        os.makedirs('data')
+        
     n = np.sum([x.startswith(name) for x in os.listdir('data/')])
     with open(f'data/{name}-{n}.p', 'wb') as f: 
         pickle.dump(data, f)
+        
+
+def load_results(fname): 
+    results = []
+    for name in glob(f'data/{fname}*'):
+        # print(name)
+        with open(f'{name}', 'rb') as f: 
+            content = pickle.load(f)
+            results.extend(content)
+    return results 
+
               
